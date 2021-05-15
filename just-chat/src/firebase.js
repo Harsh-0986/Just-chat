d66869f5ca3682c5b226cb91499f0b3ec40db0d9
Harsh-0986 <harsh.r.shah2102@gmail.com>
2021-05-15 12:40:42 +0000

    Adding firebase to the app

---
+++ just-chat/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD2XRTitKfZtgbdxmj_Ow6JMeYmJoMN11o",
+    authDomain: "just-chat-30892.firebaseapp.com",
+    projectId: "just-chat-30892",
+    storageBucket: "just-chat-30892.appspot.com",
+    messagingSenderId: "13743925213",
+    appId: "1:13743925213:web:908531aba833ba04033834"
+  };
