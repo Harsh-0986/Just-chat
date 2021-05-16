0e1612b1b4f3d57a28befc52576bd64af85ce54a
Harsh-0986 <harsh.r.shah2102@gmail.com>
2021-05-16 07:23:50 +0000

    Completing the search bar

--- just-chat/src/components/Sidebar.js
+++ just-chat/src/components/Sidebar.js
@@ -4,6 +4,7 @@ import { Avatar, IconButton } from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
 
 function Sidebar() {
   return (
@@ -23,7 +24,12 @@ function Sidebar() {
         </div>
       </div>
 
-      <div className="sidebar__search"></div>
+      <div className="sidebar__search">
+        <div className="sidebar__searchContainer">
+          <SearchOutlined />
+          <input placeholder="Search or start a new chat" type="text" />
+        </div>
+      </div>
 
       <div className="sidebar__chats"></div>
     </div>
