afdc7270d7d839ac409c19b6c8585e9b37288662
Harsh-0986 <harsh.r.shah2102@gmail.com>
2021-05-17 05:43:21 +0000

    Add firebase config

--- just-chat/src/firebase.js
+++ just-chat/src/firebase.js
@@ -1,8 +1,14 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyD2XRTitKfZtgbdxmj_Ow6JMeYmJoMN11o",
-    authDomain: "just-chat-30892.firebaseapp.com",
-    projectId: "just-chat-30892",
-    storageBucket: "just-chat-30892.appspot.com",
-    messagingSenderId: "13743925213",
-    appId: "1:13743925213:web:908531aba833ba04033834"
-  };
+  apiKey: "AIzaSyD2XRTitKfZtgbdxmj_Ow6JMeYmJoMN11o",
+  authDomain: "just-chat-30892.firebaseapp.com",
+  projectId: "just-chat-30892",
+  storageBucket: "just-chat-30892.appspot.com",
+  messagingSenderId: "13743925213",
+  appId: "1:13743925213:web:908531aba833ba04033834",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
