70c8b5954e2acfeb8cb29fe7c791d32ca48dd0ce
Harsh-0986 <harsh.r.shah2102@gmail.com>
2021-05-17 10:34:18 +0000

    Show messages from database

--- just-chat/src/App.js
+++ just-chat/src/App.js
@@ -1,13 +1,23 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
       <div className="app__body">
-        <Sidebar />
-        <Chat />
+        <Router>
+          <Sidebar />
+          <Switch>
+            <Route path="/rooms/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <Chat />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     </div>
   );
