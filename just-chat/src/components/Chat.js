7a80a0ac3b773c0d01dc9a05764e3c8650193cff
Harsh-0986 <harsh.r.shah2102@gmail.com>
2021-05-17 05:23:04 +0000

    Creating the footer

--- just-chat/src/components/Chat.js
+++ just-chat/src/components/Chat.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "../css/Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import { MoreVert, AttachFile, SearchOutlined } from "@material-ui/icons";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 
 function Chat() {
-  return <div></div>;
+  const [seed, setSeed] = useState("");
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <div className="chat__headerInfo">
+          <h3>Chat name</h3>
+          <p>Last seen</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body">
+        <p className={`chat__message ${true && "chat__receiver"}`}>
+          <span className="chat__name">Harsh Shah</span>
+          Example message
+          <span className="chat__timestamp">10:30am</span>
+        </p>
+      </div>
+
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form>
+          <input type="text" placeholder="Type a message" />
+          <button>Send</button>
+        </form>
+        <MicIcon />
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
