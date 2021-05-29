4c7395950b8023f06b0e59a7581293a1122ba57e
Harsh Shah <harsh.r.shah2102@gmail.com>
2021-05-29 18:32:44 +0530

    Showing messages based on room

--- just-chat/src/components/Chat.js
+++ just-chat/src/components/Chat.js
@@ -12,12 +12,21 @@ function Chat() {
   const [input, setInput] = useState("");
   const { roomId } = useParams();
   const [roomName, setRoomName] = useState("");
+  const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     if (roomId) {
       db.collection("rooms")
         .doc(roomId)
         .onSnapshot((snapshot) => setRoomName(snapshot.data().name));
+
+      db.collection("rooms")
+        .doc(roomId)
+        .collection("messages")
+        .orderBy("timestamp", "asc")
+        .onSnapshot((snapshot) =>
+          setMessages(snapshot.docs.map((doc) => doc.data()))
+        );
     }
   }, [roomId]);
 
@@ -53,11 +62,15 @@ function Chat() {
       </div>
 
       <div className="chat__body">
-        <p className={`chat__message ${true && "chat__receiver"}`}>
-          <span className="chat__name">Harsh Shah</span>
-          Example message
-          <span className="chat__timestamp">10:30am</span>
-        </p>
+        {messages.map((message) => (
+          <p className={`chat__message ${true && "chat__receiver"}`}>
+            <span className="chat__name">{message.name}</span>
+            {message.message}
+            <span className="chat__timestamp">
+              {new Date(message.timestamp?.toDate()).toUTCString()}
+            </span>
+          </p>
+        ))}
       </div>
 
       <div className="chat__footer">
